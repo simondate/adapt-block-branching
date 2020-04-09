4dc27f0c5d97e78a6222b963d92345216975efca
Simon Date <simondate@yahoo.co.uk>
2020-04-09 09:39:24 -0500

    Article level _isEnabled now works

--- js/adapt-block-branching.js
+++ js/adapt-block-branching.js
@@ -1,41 +1,38 @@
 define([
-    'coreJS/adapt',
-    'coreViews/blockView',
-    'coreModels/blockModel',
-    'coreModels/articleModel',
-    './adapt-block-branchingBlockView',
-    './adapt-block-branchingBlockModel',
-    './adapt-block-branchingArticleModel',
-], function (Adapt, CoreBlockView, CoreBlockModel, CoreArticleModel, BlockView, BlockModel, ArticleModel) {
+  'coreJS/adapt',
+  'coreViews/blockView',
+  'coreModels/blockModel',
+  'coreModels/articleModel',
+  './adapt-block-branchingBlockView',
+  './adapt-block-branchingBlockModel',
+  './adapt-block-branchingArticleModel',
+], function(Adapt, CoreBlockView, CoreBlockModel, CoreArticleModel, BlockView, BlockModel, ArticleModel) {
 
-    var BRANCHING_ID = "_scenario";
+  var BRANCHING_ID = "_scenario";
 
-    var BlockViewInitialize = CoreBlockView.prototype.initialize;
-    CoreBlockView.prototype.initialize = function (options) {
-        if (this.model.get(BRANCHING_ID) && this.model.get(BRANCHING_ID)._isEnabled) {
-            //extend
-            _.extend(this, BlockView);
-        }
-        //initialize the block in the normal manner
-        return BlockViewInitialize.apply(this, arguments);
-    };
+  var BlockViewInitialize = CoreBlockView.prototype.initialize;
+  CoreBlockView.prototype.initialize = function(options) {
+    if (this.model.get(BRANCHING_ID) && this.model.get(BRANCHING_ID)._isEnabled) {
+      _.extend(this, BlockView);
+    }
+    return BlockViewInitialize.apply(this, arguments);
+  };
 
-    var BlockModelInitialize = CoreBlockModel.prototype.initialize;
-    CoreBlockModel.prototype.initialize = function (options) {
-        if (this.get(BRANCHING_ID) && this.get(BRANCHING_ID)._isEnabled) {
-            //extend
-            _.extend(this, BlockModel);
-        }
-        return BlockModelInitialize.apply(this, arguments);
-    };
+  var BlockModelInitialize = CoreBlockModel.prototype.initialize;
+  CoreBlockModel.prototype.initialize = function(options) {
+    if (this.get(BRANCHING_ID) && this.get(BRANCHING_ID)._isEnabled) {
+      _.extend(this, BlockModel);
+    }
+    return BlockModelInitialize.apply(this, arguments);
+  };
 
-    var ArticleModelInitialize = CoreArticleModel.prototype.initialize;
-    CoreArticleModel.prototype.initialize = function (options) {
-        _.extend(this, ArticleModel);
-        this._setup();
+  var ArticleModelInitialize = CoreArticleModel.prototype.initialize;
+  CoreArticleModel.prototype.initialize = function(options) {
+    if (this.get(BRANCHING_ID) && this.get(BRANCHING_ID)._isEnabled) {
+      _.extend(this, ArticleModel);
+      this._setup();
+    }
 
-        return ArticleModelInitialize.apply(this, arguments);
-
-    };
-
+    return ArticleModelInitialize.apply(this, arguments);
+  };
 });
